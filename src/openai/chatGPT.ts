f119b74b2a0c2b10839927b2ed6d38d3d4d572e5
Joshua Pham <joshua.pham@gmail.com>
2023-10-01 20:06:34 -0400

    formatting

--- src/openai/chatGPT.ts
+++ src/openai/chatGPT.ts
@@ -1,7 +1,7 @@
-import { request, RequestUrlParam } from 'obsidian';
-import { openai } from './chatGPT-types';
+import { request, RequestUrlParam } from 'obsidian'
+import { openai } from './chatGPT-types'
 
-export const OPENAI_COMPLETIONS_URL = `https://api.openai.com/v1/chat/completions`;
+export const OPENAI_COMPLETIONS_URL = `https://api.openai.com/v1/chat/completions`
 
 export const CHAT_MODELS = {
 	GPT35: {
@@ -20,14 +20,14 @@ export const CHAT_MODELS = {
 		name: 'gpt-4-32k',
 		tokenLimit: 32768
 	}
-};
+}
 
-export type ChatGPTModel = typeof CHAT_MODELS.GPT35 | typeof CHAT_MODELS.GPT4;
+export type ChatGPTModel = typeof CHAT_MODELS.GPT35 | typeof CHAT_MODELS.GPT4
 
-export type ChatGPTModelType = keyof typeof CHAT_MODELS;
+export type ChatGPTModelType = keyof typeof CHAT_MODELS
 
 export function chatModelByName(name: string) {
-	return Object.values(CHAT_MODELS).find((model) => model.name === name);
+	return Object.values(CHAT_MODELS).find((model) => model.name === name)
 }
 
 export const defaultChatGPTSettings: Partial<openai.CreateChatCompletionRequest> =
@@ -39,7 +39,7 @@ export const defaultChatGPTSettings: Partial<openai.CreateChatCompletionRequest>
 		presence_penalty: 0,
 		frequency_penalty: 0,
 		stop: []
-	};
+	}
 
 export async function getChatGPTCompletion(
 	apiKey: string,
@@ -53,33 +53,33 @@ export async function getChatGPTCompletion(
 	const headers = {
 		Authorization: `Bearer ${apiKey}`,
 		'Content-Type': 'application/json'
-	};
+	}
 	const body: openai.CreateChatCompletionRequest = {
 		messages,
 		model,
 		...settings
-	};
+	}
 	const requestParam: RequestUrlParam = {
 		url: apiUrl,
 		method: 'POST',
 		contentType: 'application/json',
 		body: JSON.stringify(body),
 		headers
-	};
-	console.debug('Calling openAI', requestParam);
+	}
+	console.debug('Calling openAI', requestParam)
 	const res: openai.CreateChatCompletionResponse | undefined = await request(
 		requestParam
 	)
 		.then((response) => {
-			return JSON.parse(response);
+			return JSON.parse(response)
 		})
 		.catch((err) => {
-			console.error(err);
+			console.error(err)
 			if (err.code === 429) {
 				console.error(
 					'OpenAI API rate limit exceeded. If you have free account, your credits may have been consumed or expired.'
-				);
+				)
 			}
-		});
-	return res?.choices?.[0]?.message?.content;
+		})
+	return res?.choices?.[0]?.message?.content
 }
